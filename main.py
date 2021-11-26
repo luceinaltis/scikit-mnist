1d5c16df71aef3fbcf8d055d00d1c963a5ad1523
김기환 <kihwan@gimgihwan-ui-iMac.local>
2021-11-26 16:45:27 +0900

    report

--- main.py
+++ main.py
@@ -1,50 +1,57 @@
 import matplotlib.pyplot as plt
+import time
 
 from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+import tensorflow as tf 
 
-digits = datasets.load_digits()
+import keras
+from keras.models import Sequential,Input,Model
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers.advanced_activations import LeakyReLU
 
-print(len(digits.images))
+dataset = datasets.load_digits()
+x_data = dataset.data
+y_data = dataset.target
 
-_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, label in zip(axes, digits.images, digits.target):
-    ax.set_axis_off()
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-    ax.set_title("Training: %i" % label)
-    
-# flatten the images
-n_samples = len(digits.images)
-data = digits.images.reshape((n_samples, -1))
+img_rows, img_cols=8, 8
 
-# Create a classifier: a support vector classifier
-clf = svm.SVC(gamma=0.001)
+x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.3)
 
-# Split data into 70% train and 30% test subsets
-X_train, X_test, y_train, y_test = train_test_split(
-    data, digits.target, test_size=0.3, shuffle=False
-)
+x_train = x_train.reshape(-1, 8,8, 1)
+x_test = x_test.reshape(-1, 8,8, 1)
+print(x_train.shape)
 
-# Learn the digits on the train subset
-clf.fit(X_train, y_train)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train = x_train / 255.
+x_test = x_test / 255.
 
-# Predict the value of the digit on the test subset
-predicted = clf.predict(X_test)
+train_Y_one_hot = tf.keras.utils.to_categorical(y_train)
+test_Y_one_hot = tf.keras.utils.to_categorical(y_test)
 
-_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, prediction in zip(axes, X_test, predicted):
-    ax.set_axis_off()
-    image = image.reshape(8, 8)
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-    ax.set_title(f"Prediction: {prediction}")
-    
-print(
-    f"Classification report for classifier {clf}:\n"
-    f"{metrics.classification_report(y_test, predicted)}\n"
-)
+model = Sequential()
+model.add(Conv2D(4, kernel_size=(3, 3),activation='linear',input_shape=(8,8,1),padding='same'))
+model.add(LeakyReLU(alpha=0.1))
+model.add(MaxPooling2D((2, 2),padding='same'))
+model.add(Flatten())
+model.add(Dense(36, activation='linear'))
+model.add(LeakyReLU(alpha=0.1))                  
+model.add(Dense(10, activation='softmax'))
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer=tf.optimizers.Adam(),metrics=['accuracy'])
 
-disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
-disp.figure_.suptitle("Confusion Matrix")
-print(f"Confusion matrix:\n{disp.confusion_matrix}")
+model.fit(x_train, train_Y_one_hot, epochs=3000, batch_size=512)
 
-plt.show()
+
+start_time = time.process_time_ns()
+score = model.evaluate(x_test, test_Y_one_hot, verbose=0)
+end_time = time.process_time_ns()
+print("Logistic Regression: ", end_time - start_time, "ns")
+
+print('loss=', score[0])
+print('accuracy=', score[1])
